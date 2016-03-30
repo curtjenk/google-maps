cfc9cc257a29297945be789daefa2b2d4e21071c
curtjenk <curtjenk@comcast.net>
2016-03-30 13:22:29 -0400

    Lots of changes
    Reused the google info window so only one window can be opened.
    Removed ngDialog in favor of using googles info window.  Added marker
    animation.

--- js/controller.js
+++ js/controller.js
@@ -1,24 +1,13 @@
-var mapsApp = angular.module('mapsApp', ['ngDialog']);
-mapsApp.controller('mapsController', function($scope, ngDialog) {
+var mapsApp = angular.module('mapsApp', []);
+mapsApp.controller('mapsController', function($scope) {
 
+    $scope.markers = [];
+    $scope.cities = cities;
 
-    $scope.showInfo = function(city) {
-        var contentString = '<div id="content">' +
-            '<div id="siteNotice">' +
-            '</div>' +
-            '<h1 id="firstHeading" class="firstHeading">' + city.city + '</h1>' +
-            '<div id="bodyContent">' +
-            '<p><b>' + city.city + ', ' + city.state + '</b></p>' +
-            '<p>Population is ' + city.yearEstimate + ' which is ' + city.change +
-            ' over the last census count of ' + city.lastCensus + '</p>' +
-            '<p>' + city.city + ' occupies ' + city.landArea + ' ( density = ' + city.lastPopDensity + ')</p>' +
-            '</div>' +
-            '</div>';
-        ngDialog.open({
-            template: contentString,
-            plain: true,
-            className: 'ngdialog-theme-default'
-        });
+    $scope.showInfoClick = function(mNdx) {
+        //trigger a click event on a particular marker
+        $scope.markers[mNdx].setAnimation(google.maps.Animation.DROP);
+        google.maps.event.trigger($scope.markers[mNdx], 'click');
     };
 
     $scope.map = new google.maps.Map(document.getElementById('map'), {
@@ -26,38 +15,50 @@ mapsApp.controller('mapsController', function($scope, ngDialog) {
         center: new google.maps.LatLng(40.0000, -98.0000)
     });
 
+
     function createMarker(city) {
         // console.log(city);
         var latLon = city.latLon.split(',', 2);
         var lat = Number(latLon[0]);
         var lon = Number(latLon[1]);
-        var contentString = '<div id="content">' +
-            '<div id="siteNotice">' +
-            '</div>' +
-            '<h1 id="firstHeading" class="firstHeading">' + city.city + '</h1>' +
-            '<div id="bodyContent">' +
-            '<p><b>' + city.city + ', ' + city.state + '</b></p>' +
-            '<p>Population is ' + city.yearEstimate + ' which is ' + city.change +
-            ' over the last census count of ' + city.lastCensus + '</p>' +
-            '<p>' + city.city + ' occupies ' + city.landArea + ' ( density = ' + city.lastPopDensity + ')</p>' +
-            '</div>' +
-            '</div>';
+        var contentString = getContentString(city);
 
         var marker = new google.maps.Marker({
             position: new google.maps.LatLng(lat, lon),
             map: $scope.map,
-            title: city.city + ', ' + city.state
+            title: city.city + ', ' + city.state,
+            animation: google.maps.Animation.DROP
         });
 
-        var infowindow = new google.maps.InfoWindow({
-            content: contentString
-        });
         marker.addListener('click', function() {
+            infowindow.setContent(contentString);
             infowindow.open($scope.map, marker);
         });
+
+        $scope.markers.push(marker);
     }
 
-    $scope.cities = cities;
+    var infowindow = new google.maps.InfoWindow({
+        // content: contentString
+    });
+
+    function getContentString(city) {
+        var contentString = '<div id="content">' +
+            '<div id="siteNotice">' +
+            '</div>' +
+            '<h1 class="firstHeading">' + city.city + '</h1>' +
+            '<div class="bodyContent">' +
+            '<div><strong>Total Population : </strong>' + city.yearEstimate + '</div> ' +
+            '<div><strong>2010 Census : </strong>' + city.lastCensus + '</div> ' +
+            '<div><strong>Population Change : </strong>' + city.change + ' </div> ' +
+            '<div><strong>Population Density : </strong>' + city.lastPopDensity + ' </div> ' +
+            '<div><strong>State : </strong>' + city.state + ' </div> ' +
+            '<div><strong>Land Area : </strong>' + city.landArea + '</div>' +
+            '</div>' +
+            '</div>';
+        return contentString;
+    }
+
 
     for (var i = 0; i < cities.length; i++) {
         createMarker(cities[i]);
