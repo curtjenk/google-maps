eb9520aa3cea7470863fec779c90ac4702c192f7
joecih <joecih@gmail.com>
2016-04-01 08:17:13 -0400

    Added in material design features and updated controller.js

--- js/controller.js
+++ js/controller.js
@@ -1,258 +1,329 @@
-var mapsApp = angular.module('mapsApp',
-[
+var mapsApp = angular.module('mapsApp', [
   'ngRoute',
-  'angularjs-dropdown-multiselect'
+  'angularjs-dropdown-multiselect',
+  'ngMaterial',
+  // 'ngMessages',
+  // 'material.svgAssetsCache'
 ]);
 
-mapsApp.controller('mapsController', function($scope, $compile) {
+mapsApp.controller('mapsController', function($scope, $compile, $timeout, $mdSidenav, $log) {
 
 
-  var originalCenter = new google.maps.LatLng(40.0000, -98.0000);
-  var originalZoom = 4;
-  var clickZoom = 6;
-  var finalZoom = 11;
-  var directionsService = new google.maps.DirectionsService;
-  var directionsDisplay = new google.maps.DirectionsRenderer;
+    var originalCenter = new google.maps.LatLng(40.0000, -98.0000);
+    var originalZoom = 4;
+    var clickZoom = 6;
+    var finalZoom = 11;
+    var directionsService = new google.maps.DirectionsService;
+    var directionsDisplay = new google.maps.DirectionsRenderer;
 
+    $scope.toggleLeft = buildDelayedToggler('left');
+    $scope.toggleRight = buildToggler('right');
+    $scope.isOpenRight = function(){
+      return $mdSidenav('right').isOpen();
+    };
 
-  $scope.markers = [];
-  $scope.cities = cities;
+    $scope.markers = [];
+    $scope.cities = cities;
 
-  $scope.map = new google.maps.Map(document.getElementById('map'), {
-    zoom: originalZoom,
-    center: originalCenter
-  });
+    $scope.map = new google.maps.Map(document.getElementById('map'), {
+      zoom: originalZoom,
+      center: originalCenter
+    });
 
-  directionsDisplay.setMap($scope.map);
+    directionsDisplay.setMap($scope.map);
 
-  $scope.example1model = [];
-  $scope.example1data = normalizedPlaces(); //[ {id: 1, label: "David"}, {id: 2, label: "Jhon"}, {id: 3, label: "Danny"}];
-  $scope.example5customTexts = {buttonDefaultText: 'Place Types'};
-  $scope.example10settings = {selectionLimit: 5};
+    $scope.example1model = [];
+    $scope.example1data = normalizedPlaces(); //[ {id: 1, label: "David"}, {id: 2, label: "Jhon"}, {id: 3, label: "Danny"}];
+    $scope.example5customTexts = {
+      buttonDefaultText: 'Place Types'
+    };
+    $scope.example10settings = {
+      selectionLimit: 5
+    };
 
-  $scope.onChangeHandler = function(_latLonValue) {
-    calculateAndDisplayRoute(directionsService, directionsDisplay, _latLonValue);
-  };
+    $scope.onChangeHandler = function(_latLonValue) {
+      calculateAndDisplayRoute(directionsService, directionsDisplay, _latLonValue);
+    };
 
-  $scope.showInfoClick = function(mNdx) {
-    var _aPlaceTypes = [];
+    $scope.showInfoClick = function(mNdx) {
+      var _aPlaceTypes = [];
 
-    for (i=0; i < $scope.example1model.length; i++) {
-      _aPlaceTypes.push($scope.example1data[ $scope.example1model[i].id ].original);
+      for (i = 0; i < $scope.example1model.length; i++) {
+        _aPlaceTypes.push($scope.example1data[$scope.example1model[i].id].original);
+      }
+
+      //console.log(_aPlaceTypes);
+
+      if (_aPlaceTypes.length > 0) {
+        // what we want to do?
+        placesSearch(mNdx, _aPlaceTypes);
+      } else {
+        //trigger a click event on a particular marker
+        $scope.markers[mNdx].setAnimation(google.maps.Animation.DROP);
+        google.maps.event.trigger($scope.markers[mNdx], 'click');
+      }
+    };
+
+    function placesSearch(ndxOfCityClicked, _arr) {
+      //pyrmont should be the location/city selected,
+      var cityLocation = {
+        lat: $scope.markers[ndxOfCityClicked].position.lat(),
+        lng: $scope.markers[ndxOfCityClicked].position.lng()
+      };
+
+      ///???? do we really need to create a new map?
+      //  $scope.map  = new google.maps.Map(document.getElementById('map'), {
+      //      center: cityLocation,
+      //      zoom: 10
+      //  });
+
+      $scope.map.setCenter(cityLocation);
+      $scope.map.setZoom(10);
+
+      var service = new google.maps.places.PlacesService($scope.map);
+
+      console.log(_arr);
+      service.nearbySearch({
+        location: cityLocation,
+        radius: 50000,
+        type: _arr //use array of selected placesTypes
+      }, placesSearchcallback);
     }
 
-    //console.log(_aPlaceTypes);
+    function placesSearchcallback(results, status) {
+      //console.log(results);
+      if (status === google.maps.places.PlacesServiceStatus.OK) {
+        for (var i = 0; i < results.length; i++) {
+          createPlacesMarker(results[i]);
+        }
+      }
+    }
 
-    if ( _aPlaceTypes.length > 0 ) {
-       // what we want to do?
-       placesSearch(mNdx, _aPlaceTypes);
-    } else {
+    function createPlacesMarker(place) {
+      var placeLoc = place.geometry.location;
+      var marker = new google.maps.Marker({
+        map: $scope.map,
+        position: place.geometry.location
+      });
+
+      //  google.maps.event.addListener(marker, 'click', function() {
+      //      infowindow.setContent(place.name);
+      //      infowindow.open($scope.map, this);
+      //  });
+
+      marker.addListener('click', function() {
+        infowindow.setContent(place.name); //contentString[0]);
+        infowindow.open($scope.map, marker);
+      });
+    }
+
+    var zoomAnimator = new ZoomAnimator({
+      zoomInterval: 500,
+      duration: 100
+    });
+
+    var easingAnimator = new EasingAnimator({
+      easingInterval: 500,
+      duration: 2000,
+      step: 100,
+      callBack: function(latLng) {
+        $scope.map.setCenter(latLng);
+        $scope.map.setZoom(clickZoom);
+      },
+      finalCallBack: function() {
+        console.log("Zooming in");
+        zoomAnimator.zoomIn(
+          $scope.map.getZoom(),
+          11,
+          function(zoom) {
+            console.log("IN = " + zoom);
+            $scope.map.setZoom(zoom);
+          },
+          function() {}
+        );
+      }
+    });
+
+
+
+    // var easingAnimator = EasingAnimator.makeFromCallback(function(latLng) {
+    //     $scope.map.setCenter(latLng);
+    //     $scope.map.setZoom(4);
+    // });
+
+    $scope.zoomClick = function(mNdx) {
       //trigger a click event on a particular marker
-      $scope.markers[mNdx].setAnimation(google.maps.Animation.DROP);
-      google.maps.event.trigger($scope.markers[mNdx], 'click');
-    }
-  };
+      // $scope.map.panTo($scope.markers[mNdx].position);
 
-  function placesSearch(ndxOfCityClicked, _arr) {
-     //pyrmont should be the location/city selected,
-     var cityLocation = {
-         lat: $scope.markers[ndxOfCityClicked].position.lat(),
-         lng: $scope.markers[ndxOfCityClicked].position.lng()
-     };
+      // var point = originalCenter;
+      var point = $scope.map.getCenter();
+      var destinationPoint = {
+        lat: $scope.markers[mNdx].position.lat(),
+        lng: $scope.markers[mNdx].position.lng()
+      };
+      //$scope.map.setZoom(clickZoom);
 
-     ///???? do we really need to create a new map?
-    //  $scope.map  = new google.maps.Map(document.getElementById('map'), {
-    //      center: cityLocation,
-    //      zoom: 10
-    //  });
-
-    $scope.map.setCenter(cityLocation);
-    $scope.map.setZoom(10);
-
-     var service = new google.maps.places.PlacesService($scope.map);
-
-     console.log(_arr);
-     service.nearbySearch({
-         location: cityLocation,
-         radius: 50000,
-         type: _arr //use array of selected placesTypes
-     }, placesSearchcallback);
-  }
-
-  function placesSearchcallback(results, status) {
-    //console.log(results);
-     if (status === google.maps.places.PlacesServiceStatus.OK) {
-         for (var i = 0; i < results.length; i++) {
-             createPlacesMarker(results[i]);
-         }
-     }
-  }
-
-  function createPlacesMarker(place) {
-     var placeLoc = place.geometry.location;
-     var marker = new google.maps.Marker({
-         map: $scope.map,
-         position: place.geometry.location
-     });
-
-    //  google.maps.event.addListener(marker, 'click', function() {
-    //      infowindow.setContent(place.name);
-    //      infowindow.open($scope.map, this);
-    //  });
-
-     marker.addListener('click', function() {
-       infowindow.setContent(place.name); //contentString[0]);
-       infowindow.open($scope.map, marker);
-     });
-  }
-
-  var zoomAnimator = new ZoomAnimator({
-    zoomInterval: 500,
-    duration: 100
-  });
-
-  var easingAnimator = new EasingAnimator({
-    easingInterval: 500,
-    duration: 2000,
-    step: 100,
-    callBack: function(latLng) {
-      $scope.map.setCenter(latLng);
-      $scope.map.setZoom(clickZoom);
-    },
-    finalCallBack: function() {
-      console.log("Zooming in");
-      zoomAnimator.zoomIn(
+      // easingAnimator.easeProp({
+      //   lat: point.lat(),
+      //   lng: point.lng()
+      // }, destinationPoint);
+      console.log("Starting the zoom out at " + $scope.map.getZoom() + " End=6");
+      zoomAnimator.zoomOut(
         $scope.map.getZoom(),
-        11,
+        6,
         function(zoom) {
-          console.log("IN = " + zoom);
+          console.log("OUT = " + zoom);
           $scope.map.setZoom(zoom);
         },
-        function() {}
+        function() {
+          easingAnimator.easeProp({
+            lat: point.lat(),
+            lng: point.lng()
+          }, destinationPoint);
+        }
       );
-    }
-  });
 
-
-
-  // var easingAnimator = EasingAnimator.makeFromCallback(function(latLng) {
-  //     $scope.map.setCenter(latLng);
-  //     $scope.map.setZoom(4);
-  // });
-
-  $scope.zoomClick = function(mNdx) {
-    //trigger a click event on a particular marker
-    // $scope.map.panTo($scope.markers[mNdx].position);
-
-    // var point = originalCenter;
-    var point = $scope.map.getCenter();
-    var destinationPoint = {
-      lat: $scope.markers[mNdx].position.lat(),
-      lng: $scope.markers[mNdx].position.lng()
     };
-    //$scope.map.setZoom(clickZoom);
 
-    // easingAnimator.easeProp({
-    //   lat: point.lat(),
-    //   lng: point.lng()
-    // }, destinationPoint);
-    console.log("Starting the zoom out at " + $scope.map.getZoom() + " End=6");
-    zoomAnimator.zoomOut(
-      $scope.map.getZoom(),
-      6,
-      function(zoom) {
-        console.log("OUT = " + zoom);
-        $scope.map.setZoom(zoom);
-      },
-      function() {
-        easingAnimator.easeProp({
-          lat: point.lat(),
-          lng: point.lng()
-        }, destinationPoint);
+
+    function createMarker(city) {
+      // console.log(city);
+      var _latLon = city.latLon.split(',', 2);
+      var lat = Number(_latLon[0]);
+      var lon = Number(_latLon[1]);
+      var contentString = getContentString(city);
+
+      //console.log(latLon);
+
+      var marker = new google.maps.Marker({
+        position: new google.maps.LatLng(lat, lon),
+        map: $scope.map,
+        title: city.city + ', ' + city.state,
+        animation: google.maps.Animation.DROP
+      });
+
+      marker.addListener('click', function() {
+        infowindow.setContent(contentString[0]);
+        infowindow.open($scope.map, marker);
+      });
+
+      $scope.markers.push(marker);
+    }
+
+    var infowindow = new google.maps.InfoWindow({
+      // content: contentString
+    });
+
+    function getContentString(city) {
+
+      var _content = '<div id="content">' +
+        '<div id="siteNotice">' +
+        '</div>' +
+        '<h1 class="firstHeading">' + city.city + '</h1>' +
+        '<div class="bodyContent">' +
+        '<div><strong>Total Population : </strong>' + city.yearEstimate + '</div> ' +
+        '<div><strong>2010 Census : </strong>' + city.lastCensus + '</div> ' +
+        '<div><strong>Population Change : </strong>' + city.change + ' </div> ' +
+        '<div><strong>Population Density : </strong>' + city.lastPopDensity + ' </div> ' +
+        '<div><strong>State : </strong>' + city.state + ' </div> ' +
+        '<div><strong>Land Area : </strong>' + city.landArea + '</div>' +
+        '<div><a href="#" ng-click="onChangeHandler(\'' + city.latLon + '\')">Directons To</a></div>' +
+        '</div>' +
+        '</div>';
+      return $compile(_content)($scope); //contentString; RETURN'd as ARRAY!!
+    }
+
+
+    for (var i = 0; i < cities.length; i++) {
+      createMarker(cities[i]);
+    }
+
+    function calculateAndDisplayRoute(directionsService, directionsDisplay, _latLonValue) {
+      console.log(_latLonValue);
+
+      var _latLon = _latLonValue.split(',', 2);
+
+      console.log(_latLon);
+
+      var lat = Number(_latLon[0]);
+      var lon = Number(_latLon[1]);
+
+      var _destination = new google.maps.LatLng(lat, lon);
+      var _origin = new google.maps.LatLng(33.7629, -84.4227);
+
+      directionsService.route({
+        // origin: document.getElementById('start').value,
+        // destination: document.getElementById('end').value,
+        origin: _origin,
+        destination: _destination,
+        travelMode: google.maps.TravelMode.DRIVING
+
+      }, function(response, status) {
+        if (status === google.maps.DirectionsStatus.OK) {
+          directionsDisplay.setDirections(response);
+        } else {
+          window.alert('Directions request failed due to ' + status);
+        }
+      });
+    }
+
+    /**
+     * Supplies a function that will continue to operate until the
+     * time is up.
+     */
+    function debounce(func, wait, context) {
+      var timer;
+
+      return function debounced() {
+        var context = $scope,
+          args = Array.prototype.slice.call(arguments);
+        $timeout.cancel(timer);
+        timer = $timeout(function() {
+          timer = undefined;
+          func.apply(context, args);
+        }, wait || 10);
+      };
+    }
+
+    /**
+     * Build handler to open/close a SideNav; when animation finishes
+     * report completion in console
+     */
+    function buildDelayedToggler(navID) {
+      return debounce(function() {
+        $mdSidenav(navID)
+          .toggle()
+          .then(function() {
+            $log.debug("toggle " + navID + " is done");
+          });
+      }, 200);
+    }
+
+    function buildToggler(navID) {
+      return function() {
+        $mdSidenav(navID)
+          .toggle()
+          .then(function() {
+            $log.debug("toggle " + navID + " is done");
+          });
       }
-    );
+    }
 
-  };
-
-
-  function createMarker(city) {
-    // console.log(city);
-    var _latLon = city.latLon.split(',', 2);
-    var lat = Number(_latLon[0]);
-    var lon = Number(_latLon[1]);
-    var contentString = getContentString(city);
-
-    //console.log(latLon);
-
-    var marker = new google.maps.Marker({
-      position: new google.maps.LatLng(lat, lon),
-      map: $scope.map,
-      title: city.city + ', ' + city.state,
-      animation: google.maps.Animation.DROP
-    });
-
-    marker.addListener('click', function() {
-      infowindow.setContent(contentString[0]);
-      infowindow.open($scope.map, marker);
-    });
-
-    $scope.markers.push(marker);
-  }
-
-  var infowindow = new google.maps.InfoWindow({
-    // content: contentString
+  })
+  // .controller('LeftCtrl', function($scope, $timeout, $mdSidenav, $log) {
+  //   $scope.close = function() {
+  //     $mdSidenav('left').close()
+  //       .then(function() {
+  //         $log.debug("close LEFT is done");
+  //       });
+  //
+  //   };
+  // })
+  .controller('RightCtrl', function($scope, $timeout, $mdSidenav, $log) {
+    $scope.close = function() {
+      $mdSidenav('right').close()
+        .then(function() {
+          $log.debug("close RIGHT is done");
+        });
+    };
   });
-
-  function getContentString(city) {
-
-    var _content = '<div id="content">' +
-      '<div id="siteNotice">' +
-      '</div>' +
-      '<h1 class="firstHeading">' + city.city + '</h1>' +
-      '<div class="bodyContent">' +
-      '<div><strong>Total Population : </strong>' + city.yearEstimate + '</div> ' +
-      '<div><strong>2010 Census : </strong>' + city.lastCensus + '</div> ' +
-      '<div><strong>Population Change : </strong>' + city.change + ' </div> ' +
-      '<div><strong>Population Density : </strong>' + city.lastPopDensity + ' </div> ' +
-      '<div><strong>State : </strong>' + city.state + ' </div> ' +
-      '<div><strong>Land Area : </strong>' + city.landArea + '</div>' +
-      '<div><a href="#" ng-click="onChangeHandler(\'' + city.latLon + '\')">Directons To</a></div>' +
-      '</div>' +
-      '</div>';
-    return $compile(_content)($scope); //contentString; RETURN'd as ARRAY!!
-  }
-
-
-  for (var i = 0; i < cities.length; i++) {
-    createMarker(cities[i]);
-  }
-
-  function calculateAndDisplayRoute(directionsService, directionsDisplay, _latLonValue) {
-    console.log(_latLonValue);
-
-    var _latLon = _latLonValue.split(',', 2);
-
-    console.log(_latLon);
-
-    var lat = Number(_latLon[0]);
-    var lon = Number(_latLon[1]);
-
-    var _destination = new google.maps.LatLng(lat, lon);
-    var _origin = new google.maps.LatLng(33.7629, -84.4227);
-
-    directionsService.route({
-      // origin: document.getElementById('start').value,
-      // destination: document.getElementById('end').value,
-      origin: _origin,
-      destination: _destination,
-      travelMode: google.maps.TravelMode.DRIVING
-
-    }, function(response, status) {
-      if (status === google.maps.DirectionsStatus.OK) {
-        directionsDisplay.setDirections(response);
-      } else {
-        window.alert('Directions request failed due to ' + status);
-      }
-    });
-  }
-});
