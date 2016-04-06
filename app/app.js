05338520c9a827d8b82e933990ae3431f0ce792f
joecih <joecih@gmail.com>
2016-04-06 08:23:34 -0400

    Updating compass config file for directory structure. Added in navBar component into index.html above viewport

--- app/app.js
+++ app/app.js
@@ -25,22 +25,75 @@ mapApp.run(function($rootScope, $location, $routeParams, mapService) {
 //Angular will take any parameter with a : in front of it as a wildcard
 //That wildcard, is what the $routeParams will use above as it's property
 mapApp.config(function($routeProvider) {
-  $routeProvider.when('/', {
-    templateUrl: 'views/front.html',
-    controller: 'mapController'
-  });
-  $routeProvider.when('/city/:cityIndex', {
-    templateUrl: 'views/city.html',
-    controller: 'cityController'
-  });
+  // $routeProvider.when('/', {
+  //   templateUrl: 'views/front.html',
+  //   controller: 'mapController'
+  // });
+  // $routeProvider.when('/city/:cityIndex', {
+  //   templateUrl: 'views/city.html',
+  //   controller: 'cityController'
+  // });
   $routeProvider.otherwise({
     redirectTo: '/',
   });
 });
 
-// mapApp.controller('mainController', function($scope) {
-// 	// MAIN CONTROLLER - PLACEHOLDER
-// 	$scope.dirClick = function(lat, lon){
-// 		console.log('here');
-// 	}
-// });
+mapApp.controller('homeController', function($scope, $compile, $timeout,
+    $mdSidenav, $log) {
+
+    $scope.toggleLeft = buildDelayedToggler('left');
+    $scope.toggleRight = buildToggler('right');
+    $scope.isOpenRight = function() {
+      return $mdSidenav('right').isOpen();
+    };
+
+    /**
+     * Supplies a function that will continue to operate until the
+     * time is up.
+     */
+    function debounce(func, wait, context) {
+      var timer;
+
+      return function debounced() {
+        var context = $scope,
+          args = Array.prototype.slice.call(arguments);
+        $timeout.cancel(timer);
+        timer = $timeout(function() {
+          timer = undefined;
+          func.apply(context, args);
+        }, wait || 10);
+      };
+    }
+
+    /**
+     * Build handler to open/close a SideNav; when animation finishes
+     * report completion in console
+     */
+    function buildDelayedToggler(navID) {
+      return debounce(function() {
+        $mdSidenav(navID)
+          .toggle()
+          .then(function() {
+            $log.debug("toggle " + navID + " is done");
+          });
+      }, 200);
+    }
+
+    function buildToggler(navID) {
+      return function() {
+        $mdSidenav(navID)
+          .toggle()
+          .then(function() {
+            $log.debug("toggle " + navID + " is done");
+          });
+      };
+    }
+  })
+  .controller('RightCtrl', function($scope, $timeout, $mdSidenav, $log) {
+    $scope.close = function() {
+      $mdSidenav('right').close()
+        .then(function() {
+          $log.debug("close RIGHT is done");
+        });
+    };
+  });
